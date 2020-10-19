2d024614d23750733d1f9c4c26655011e9725d01
Fuxlor <73135866+Fuxlor@users.noreply.github.com>
2020-10-19 22:53:11 +0200

    Add files via upload
    Enregister le 19/10/2020 a 22:53

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Site En Développement</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="Header">
+        <img class="Logo" src="img/Logo-Forum" width=100 height=100>
+        <p class="Titre">
+            "Titre"
+        </p>
+    </header>
+</body>
+</html>
