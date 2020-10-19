0bd393d64f864d2427a2c4c04fffcbb61cd45dda
NeeZiaa <73132970+NeeZiaa@users.noreply.github.com>
2020-10-19 22:37:23 +0200

    J'ai fait le menu de naviguation

---
+++ menu-nav.php
@@ -0,0 +1,26 @@
+<div class="btn">
+    <nav class"menu-nav">
+        <ul>
+            <li class="btn">
+                <a href="index.php">
+                    Home
+                </a>
+            </li>
+            <li class="btn">
+                <a href="connexion.php">
+                    Connexion
+                </a>
+            </li>
+            <li class="btn">
+                <a href="infos.php">
+                    Infos sur Fn
+                </a>
+            </li>
+            <li>
+                <a href="News.php">
+                    News
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
