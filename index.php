74301fc32400338a6dd0163f277ea1c4bb2db757
Fuxlor <73135866+Fuxlor@users.noreply.github.com>
2020-10-20 11:09:01 +0200

    Update index.php
    Enregistrer le 20/10/2020 a 11:10

--- index.php
+++ index.php
@@ -1,16 +1,35 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <title>Site En Développement</title>
+    <meta charset="utf-8">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <header class="Header">
-        <img class="Logo" src="img/Logo-Forum" width=100 height=100>
-        <p class="Titre">
-            "Titre"
-        </p>
-    </header>
-</body>
+     <div id="content">
+         <div class="rightbox">                
+             <table>
+                <tr>
+                    <td class="nav">
+                        <?php include 'includes/menu-nav.php'; ?>
+                    </td>
+                     <td>
+                         <img class="Logo" src="img/Logo-Forum" width=150 height=150>
+                     </td>
+                     <td>
+                         <h1 class=Titre>
+                                "Titre"
+                         </h1>                   
+                    </td>
+                    <td>
+                         <a href="https://discord.gg/gpDZ6gM" class="discord" target="_BLANK">
+                            <img src="img/Discord-new-logo.png" width=35 height=35>
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</body> 
 </html>
+
