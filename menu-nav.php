218d28f5c79c1641438be9e0e116fcc2f716fd4d
Fuxlor <73135866+Fuxlor@users.noreply.github.com>
2020-10-20 16:32:35 +0200

    Update menu-nav.php

--- menu-nav.php
+++ menu-nav.php
@@ -1,5 +1,6 @@
-<div class="btn">
-    <nav class"menu-nav">
+<div class="left-panel">
+<img class="Logo" src = "img/Logo-Forum2.png" width = 160 height = 130 >
+    <nav class="menu-nav">
         <ul>
             <li class="btn">
                 <a href="index.php">
@@ -13,10 +14,10 @@
             </li>
             <li class="btn">
                 <a href="infos.php">
-                    Infos sur Fn
+                    Infos sur NF
                 </a>
             </li>
-            <li>
+            <li class="btn">
                 <a href="News.php">
                     News
                 </a>
