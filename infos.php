f7a746f1744bb713188afb827affc817183942bc
Fuxlor <73135866+Fuxlor@users.noreply.github.com>
2020-10-20 16:23:22 +0200

    Add files via upload
    Enregistrer le 20/10/2020 a 16:23
    
    Page "Infos sur NF"

---
+++ infos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr" class="infos">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test d'affichage</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <aside>
+         <div>
+            <?php include 'includes/menu-nav.php'; ?>
+        </div>
+    </aside>
+
+    <main>
+        <div class="titre">
+            Le NF Forum
+        </div>
+
+        <div class="texte">
+            <h1>
+                Infos Du Forum :
+            </h1>
+
+            <p>
+                 Les Fondateurs de ce forum sont Fuxlor et Neeziaa, ils ont 12 et sont fier de leur site.<br />
+                 Ce site est un forum discussion chacun peux poser une question et d'autres y répondre.
+            </p>
+        </div>
+    </main>
+</body>
+</html>
