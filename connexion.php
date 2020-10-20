c880ca5a99cbe962e0505a64dbe2f1518d2cc642
Fuxlor <73135866+Fuxlor@users.noreply.github.com>
2020-10-20 17:59:38 +0200

    Update connexion.php
    Enregistrer le 20/10/2020 a 18:00

--- connexion.php
+++ connexion.php
@@ -16,6 +16,8 @@
 <!-- Menu de naviguation -->
 <?php include 'includes/menu-nav.php' ?>
 
+    <div class="titre">Le NF Forum</div>
+
 <!-- ------------------CONNEXION--------------------------->
 <?php if (isset($_SESSION['email']) && (isset($_SESSION['pseudo']))) {
         ?>
@@ -37,11 +39,11 @@
     <h2 class="connexion">Connexion</h2>
 
     <form method="post">
-        <input class="email" type="email" name="lemail" placeholder="Email" required>
-        <input class="pseudo" type="text" name="lpseudo" placeholder="Pseudo" required>
-        <input class="mdp" type="password" name="lpassword" id="lpassword" placeholder="Mot de passe" required>
-        <input class="submit" type="submit" name="formlogin" id="formlogin" value="Connexion">
+        <input class="email" type="email" name="lemail" placeholder="Email" required><br>
+        <input class="pseudo" type="text" name="lpseudo" placeholder="Pseudo" required><br>
+        <input class="mdp" type="password" name="lpassword" id="lpassword" placeholder="Mot de passe" required><br>
         <br>
+        <input class="submit" type="submit" name="formlogin" id="formlogin" value="Connexion">
         <br>
         <br>
     </form>
@@ -55,10 +57,10 @@
     <h2 class="inscription">Inscription</h2>
  
     <form method="post">
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="text" name="pseudo" placeholder="Pseudo" required>
-        <input type="password" name="password" id="password" placeholder="Mot de passe" required>
-        <input type="password" name="cpassword" id="cpassword" placeholder="Confirmation du mot de passe" required>
+        <input type="email" name="email" placeholder="Email" required><br>
+        <input type="text" name="pseudo" placeholder="Pseudo" required><br>
+        <input type="password" name="password" id="password" placeholder="Mot de passe" required><br>
+        <input type="password" name="cpassword" id="cpassword" placeholder="Confirmation du mot de passe" required><br>
         <br>
         <input type="submit" name="formsend" id="formsend" value="Inscription">
         <br>
