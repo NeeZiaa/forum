34719954a42fd7e3836cc4b491d9c8971065427f
Fuxlor <73135866+Fuxlor@users.noreply.github.com>
2020-10-21 12:06:34 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,7 +1,7 @@
 <?php session_start(); 
 ?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" class="connect">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
