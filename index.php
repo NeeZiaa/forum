4f4a29aa85a14932d36477d9cf6853ff28e13596
Fuxlor <73135866+Fuxlor@users.noreply.github.com>
2020-10-21 14:13:38 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" class="index-m">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test d'affichage</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>NF Forum-Espace-Membre</title>
+    <link rel="stylesheet" href="css/style-membre.css">
 </head>
 <body>
-    <div class="contour">        
-        <div class="top">
-             <?php include 'includes/menu-nav.php'; ?>
+    <aside>
+         <div>
+            <?php include 'includes/menu-nav-membre.php'; ?>
         </div>
-        <div class="titre">Le FN Forum</div>
-    </div>
+    </aside>
+    <div class="titre">Le NF Forum</div>    
 </body>
 </html>
---
+++ menu-nav-membre.php
@@ -0,0 +1,22 @@
+<div class="nav-membre">
+    <img class="Logo" src = "../img/Logo-Forum2.png" width = 160 height = 130 >
+        <nav class="menu-nav-m">
+            <ul>
+                <li class="btn-m">
+                    <a href="">
+                        Forum
+                    </a>
+                </li>
+                <li class="btn-m">
+                    <a href="">
+                        News
+                    </a>
+                </li>
+                <li class="btn-m">
+                    <a href="">
+                        Videos
+                    </a>
+                </li>
+            </ul>
+        </nav>
+</div>
