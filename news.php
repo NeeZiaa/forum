868fa37d30750cd506d725ab733886a6847f9c23
Fuxlor <73135866+Fuxlor@users.noreply.github.com>
2020-10-21 12:14:07 +0200

    Add files via upload

---
+++ news.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr" class="news">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test d'affichage</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <aside>
+         <div>
+            <?php include 'includes/menu-nav.php'; ?>
+        </div>
+    </aside>
+
+    <main>
+        <div class="titre">
+            Le NF Forum
+        </div>
+
+        <div class="texte2">
+            <h1>
+                News Du Forum :
+            </h1>
+
+            <p>
+                Site en développement donc il n'y a pas de news !
+            </p>
+        </div>
+    </main>
+</body>
+</html>
