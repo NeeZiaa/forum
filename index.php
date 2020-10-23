878c8f9c71896f93b5b46c85ef429969bdc41202
Fuxlor <73135866+Fuxlor@users.noreply.github.com>
2020-10-23 12:47:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" class="index">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test d'affichage</title>
+    <title>NF Forum</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="contour">        
         <div class="top">
-             <?php include 'includes/menu-nav.php'; ?>
+            <?php include 'includes/menu-nav.php'; ?>
         </div>
-        <div class="titre">Le FN Forum</div>
+        <div class="titre">Le NF Forum</div>
     </div>
+    <div><p class="hometxt">Ici Vous trouverez un champ de discussion</p></div>
 </body>
 </html>
